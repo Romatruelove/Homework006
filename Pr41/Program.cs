dc45c00967ad8cbfe4af128d2cd0db502c6bbe7a
Roman Filimonov <romatruelove@yandex.ru>
2022-12-09 11:13:17 +0300

    Final code

---
+++ Pr41/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введи любое количество чисел через запятую:");
+string s = Console.ReadLine();
+string[] line = s.Split(',');
+int[] numbers = new int[line.Length];
+
+void CountNum()
+{
+    int count = 0; 
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = int.Parse(line[i]);
+        if(numbers[i] > 0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine($"Количество чисел больше 0 = {count}");
+}
+CountNum();
---
+++ Pr43/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите значение k1:");
+var k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение b1:");
+var b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k2:");
+var k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение b2:");
+var b2 = Convert.ToDouble(Console.ReadLine());
+void Cross()
+{
+    if (k1 == k2 && b1 == b2)
+    {
+        Console.WriteLine("Прямые совпадают!");
+    }
+    else if (k1 == k2)
+    {
+        Console.WriteLine("Прямые параллельны и не пересекаются!");
+    }
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"Точка пересечения прямых ({x};{y})");
+    }
+}
+Cross();
