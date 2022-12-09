03c8616ba1d8e0aa5ac11f42b2c8c0de8a940931
Roman Filimonov <romatruelove@yandex.ru>
2022-12-09 11:52:41 +0300

    Final code

---
+++ Pr45/Program.cs
@@ -0,0 +1,38 @@
+﻿int arraySize = 0;
+arraySize = new Random().Next(2, 15);
+int[] firstArray = new int[arraySize];
+int[] secondArray = new int[arraySize];
+ 
+void PrintArray(int[] arrayToPrint)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrayToPrint.Length; i++)
+    {
+        Console.Write("{0}", arrayToPrint[i]);
+        if (i < arrayToPrint.Length - 1)
+        {
+            Console.Write("  ");
+        }
+    }
+    Console.WriteLine("]");
+}
+ 
+void FillArray(int[] arrayToFill, int diviation)
+{
+    for (int i = 0; i < arrayToFill.Length; i++)
+        arrayToFill[i] = new Random().Next(-diviation, diviation + 1);
+}
+ 
+void CopyArray(int[] arrayToCopy, int[] arrayToFill)
+{
+    for (int i = 0; i < arrayToCopy.Length; i++)
+        arrayToFill[i] = arrayToCopy[i];
+}
+ 
+FillArray(firstArray, 20);
+CopyArray(firstArray, secondArray);
+ 
+Console.WriteLine("First array:");
+PrintArray(firstArray);
+Console.WriteLine("Second array:");
+PrintArray(secondArray);
